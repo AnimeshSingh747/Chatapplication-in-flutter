3296c8e13d58b0e0374f6f486a2e714ba9bb1e70
Animesh Singh <csanimeshsingh747@gmail.com>
2024-05-26 20:36:06 +0530

    Socket code added

--- server/server.js
+++ server/server.js
@@ -56,34 +56,34 @@ const io = require("socket.io")(server, {
   },
 });
 
-// io.on("connection", (socket) => {
-//   console.log("Sockets are in action");
-//   socket.on("setup", (userData) => {
-//     socket.join(userData._id);
-//     console.log(userData.name, "connected");
-//     socket.emit("connected");
-//   });
-//   socket.on("join chat", (room) => {
-//     socket.join(room);
-//     console.log("User joined room: " + room);
-//   });
-//   socket.on("new message", (newMessage) => {
-//     var chat = newMessage.chatId;
-//     if (!chat.users) return console.log("chat.users not defined");
+io.on("connection", (socket) => {
+  console.log("Sockets are in action");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    console.log(userData.name, "connected");
+    socket.emit("connected");
+  });
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User joined room: " + room);
+  });
+  socket.on("new message", (newMessage) => {
+    var chat = newMessage.chatId;
+    if (!chat.users) return console.log("chat.users not defined");
 
-//     chat.users.forEach((user) => {
-//       if (user._id === newMessage.sender._id) return;
-//       socket.in(user._id).emit("message received", newMessage);
-//     });
-//     socket.on("typing", (room) => {
-//       socket.in(room).emit("typing");
-//     });
-//     socket.on("stop typing", (room) => {
-//       socket.in(room).emit("stop typing");
-//     });
-//   });
-//   socket.off("setup", () => {
-//     console.log("USER DISCONNECTED");
-//     socket.leave(userData._id);
-//   });
-// });
+    chat.users.forEach((user) => {
+      if (user._id === newMessage.sender._id) return;
+      socket.in(user._id).emit("message received", newMessage);
+    });
+    socket.on("typing", (room) => {
+      socket.in(room).emit("typing");
+    });
+    socket.on("stop typing", (room) => {
+      socket.in(room).emit("stop typing");
+    });
+  });
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});
